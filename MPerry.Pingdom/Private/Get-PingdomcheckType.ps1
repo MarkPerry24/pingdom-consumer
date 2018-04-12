bf84cb0bdc3ffd1996318cc00255727c77344a33
Mark Perry <mperry@spiralsoft.local>
2018-04-12 21:43:12 +0100

    Added missing Type function

---
+++ MPerry.Pingdom/Private/Get-PingdomcheckType.ps1
@@ -0,0 +1,23 @@
+﻿Function Get-PingdomCheckType
+{
+    param
+    (
+        [string]
+        $type
+    )
+
+    $pchecktype = switch ($type)
+    {
+        "http" { [CheckHTTPParams]@{}; break }
+        "httpcustom" { [CheckHTTPCustomParams]@{}; break }
+        "tcp" { [CheckTCPUDPParams]@{}; break }
+        "ping" { [CheckPINGParams]@{}; break }
+        "dns" { [CheckDNSParams]@{}; break }
+        "smtp" { [CheckSMTParams]@{}; break }
+        "pop" { [CheckPOPIMAPParams]@{} }
+        "imap" { [CheckPOPIMAPParams]@{} }
+        default { throw "Invalid type" }
+    }
+
+    $pchecktype
+}
